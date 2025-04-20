30f22e78371633406955a73fbc041b113fd79654
marina-bm <mbonellimartinotto@gmail.com>
2025-04-19 21:27:23 -0300

    Add files via upload

---
+++ marina-bm_aula08.R
@@ -0,0 +1,18 @@
+### AVALIAÇÃO ###
+
+#Criar um documento HTML em quarto
+#Isto é, uma página de internet com seus gráficos
+
+#PRIMEIRO PASSO
+#instalar o pacote quarto
+#install.packages('quarto')
+library('quarto')
+
+#SEGUNDO PASSO
+#Criar documento ('render')
+
+#TERCEIRO PASSO: ALTERAÇÕES
+#a  cada passo clica no render
+#Alterar o tipo para seu nome
+#modificar a parte(nome técnico= R chunk) das bibliotecas
+#adicionar o carregamento dos dados
